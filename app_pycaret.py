a57e1540b37cf608ba273922ccc65ec02a905c81
Tarcisio <tarcisioalmeidabh@outlook.com>
2023-09-28 10:26:35 -0300

    update

---
+++ app_pycaret.py
@@ -0,0 +1,65 @@
+
+# Imports
+import pandas as pd
+import streamlit as st
+from io import BytesIO
+from pycaret.classification import load_model, predict_model
+
+
+@st.cache_data
+def convert_df(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+# Função para converter o df para excel
+@st.cache_data
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    writer.close()
+    processed_data = output.getvalue()
+    return processed_data
+
+
+# Função principal da aplicação
+def main():
+    # Configuração inicial da página da aplicação
+    st.set_page_config(page_title = 'PyCaret', \
+        layout="wide",
+        initial_sidebar_state='expanded'
+    )
+
+    # Título principal da aplicação
+    st.write("""## Escorando o modelo gerado no pycaret """)
+    st.markdown("---")
+    
+    # Botão para carregar arquivo na aplicação
+    st.sidebar.write("## Suba o arquivo")
+    data_file_1 = st.sidebar.file_uploader("Bank Credit Dataset", type = ['csv','ftr'])
+
+    # Verifica se há conteúdo carregado na aplicação
+    if (data_file_1 is not None):
+        df_credit = pd.read_feather(data_file_1)
+        df_credit = df_credit.sample(50000)
+
+        model_saved = load_model('C:\Users\tarci\OneDrive\Documentos\EBAC\01 - Cientista de Dados\Módulo 38 - Streamlit VI e Pycaret/exercicio_e_projeto_final')
+        predict = predict_model(model_saved, data=df_credit)
+
+        df_xlsx = to_excel(predict)
+        st.download_button(label='📥 Download',
+                            data=df_xlsx ,
+                            file_name= 'predict.xlsx')
+
+
+if __name__ == '__main__':
+	main()
+    
+
+
+
+
+
+
+
+
+
